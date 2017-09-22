4d253774a63e7d10c659703bcda8869924d33056
adstraw <adam.d.straw@intel.com>
2017-09-22 16:11:39 -0700

    object oriented ngraph::Compiler refactor  * Make nnvm::Graph a private member of ngraph::Compiler  * Remove NgraphOps_ private member from ngraph::Compiler  * Move CreateNNVMNode and create_layerGraphs out of ngraph::Compiler  * Make ngraph::Graph a private member of ngraph::Compiler  * Move init functions to ngraph::Compiler ctor

--- src/ngraph/ngraph_compiler.cc
+++ src/ngraph/ngraph_compiler.cc
@@ -7,24 +7,53 @@
 
 namespace ngraph {
 
-// Compiter initialization
-Compiler::Compiler() {
+// Function to create an nnvm node from a ngraph subgraph
+nnvm::NodeEntry CreateNNVMNode(std::shared_ptr<Graph> subgraph) {
+  // init node, set name
+  auto node = nnvm::Node::Create();
+  node->attrs.name = subgraph->name;
+  // get the registered operation for the node
+  node->attrs.op = get_subgraph_op(subgraph);
+  // setup the ninputs to the node
+  for (auto input : subgraph->inputs)
+    node->inputs.emplace_back(nnvm::NodeEntry{input->orig_node, 0, 0});
+  // create dummy node parameters
+  NGraphParam op;
+  node->attrs.parsed = std::move(op);
 
-  NgraphOps_ = compiler_.NgraphOps_;
+  // init and return NodeEntry
+  return nnvm::NodeEntry{node, 0, 0};
+}
+
+// Generator to create functions that convert mxnet layer operations
+// into a series of ngraph operations
+layerGraphs create_layerGraphs() {
+  layerGraphs layer_funcs;
+  layer_funcs[std::string("Activation")] = [](const NodePtr node) {
+    Graph tmpGraph;
+    auto act_type = node->orig_node->attrs.dict["act_type"];
+    tmpGraph.AddNode(std::make_shared<OpNode>(node->orig_node, node->name,
+                                              act_type, node->inputs));
+    return tmpGraph;
+  };
+
+  return layer_funcs;
+}
+
+// Compiler initialization
+Compiler::Compiler(const nnvm::Graph &graph) : graph_(graph) {
+  // initialize ngraph_
+  ParseNNVMGraph();
+  CheckInNGraph();
 }
 
 // Main compilation function
 nnvm::Graph Compiler::Compile(
-    nnvm::Graph graph,
     std::unordered_map<std::string, nnvm::TShape>& arg_shape_map,
     std::unordered_map<std::string, int>& arg_dtype_map,
     const nnvm::NodeEntryMap<mxnet::NDArray>& feed_dict) {
 
-  auto g = ParseNNVMGraph(graph);
-
-  CheckInNGraph(g);
-
-  g.IdentifySubgraphs(
+  ngraph_.IdentifySubgraphs(
       [&feed_dict](NodePtr s) { 
         bool in_feed_dict = false;
         for (auto kv : feed_dict){
@@ -36,13 +65,13 @@ nnvm::Graph Compiler::Compile(
         return (s->in_ngraph && s->type == NodeType::kOp && !in_feed_dict); 
       });
 
-  if (false) g.WriteSubgraphDots("pre_collapse");
-  g.CollapseSubgraphs();
+  if (false) ngraph_.WriteSubgraphDots("pre_collapse");
+  ngraph_.CollapseSubgraphs();
 
   // Output Graphviz dot files for vizualization
-  if (false) g.WriteSubgraphDots("post_collapse");
+  if (false) ngraph_.WriteSubgraphDots("post_collapse");
   // throw;
-  for (auto node : g.nodes_) {
+  for (auto node : ngraph_.nodes_) {
     // store the input variable shape for use by nnvm
     // This is happening because my nnvm graph manipulations are
     // breaking the infer shape functionality, so shapes of inputs
@@ -55,7 +84,7 @@ nnvm::Graph Compiler::Compile(
   }
 
   // find the subgraphs
-  for (auto n : g.nodes_) {
+  for (auto n : ngraph_.nodes_) {
     if (n->type == NodeType::kGraph) {
       // extract and compile subgraph
       auto sg = compiler_.Compile(n);
@@ -67,7 +96,7 @@ nnvm::Graph Compiler::Compile(
       auto name = sg->nodes_.back()->name;
 
       // use nnvm depth first search to fix node connections in nnvm
-      nnvm::DFSVisit(graph.outputs, [sg_node, &name](const nnvm::NodePtr node) {
+      nnvm::DFSVisit(graph_.outputs, [sg_node, &name](const nnvm::NodePtr node) {
         auto matches_name = [&name](nnvm::NodeEntry n) -> bool {
           return (n.node->attrs.name == name);
         };
@@ -91,15 +120,15 @@ nnvm::Graph Compiler::Compile(
   nnvm::Graph out_graph;
 
   // initialize it with original graph nodes
-  out_graph.outputs = graph.outputs;
+  out_graph.outputs = graph_.outputs;
 
   return out_graph;
 }
 
 // Check nodes in NGraph
-void Compiler::CheckInNGraph(Graph& graph) {
+void Compiler::CheckInNGraph() {
   // loop over nodes
-  for (auto node : graph.nodes_) {
+  for (auto node : ngraph_.nodes_) {
     if (node->type == NodeType::kOp) {
       //check if it's a multi output related node
       bool multioutput = false;
@@ -116,7 +145,7 @@ void Compiler::CheckInNGraph(Graph& graph) {
       if (multioutput) {
       } else {
         // if it's an operation, check operation name
-        for (auto op : NgraphOps_) {
+        for (auto op : compiler_.NgraphOps_) {
           if (node->operation == op) {
             node->in_ngraph = true;
             break;
@@ -129,60 +158,24 @@ void Compiler::CheckInNGraph(Graph& graph) {
   }
 }
 
-// Function to create an nnvm node from a ngraph subgraph
-nnvm::NodeEntry Compiler::CreateNNVMNode(std::shared_ptr<Graph> graph) {
-  // init node, set name
-  auto node = nnvm::Node::Create();
-  node->attrs.name = graph->name;
-  // get the registered operation for the node
-  node->attrs.op = get_subgraph_op(graph);
-  // setup the ninputs to the node
-  for (auto input : graph->inputs)
-    node->inputs.emplace_back(nnvm::NodeEntry{input->orig_node, 0, 0});
-  // create dummy node parameters
-  NGraphParam op;
-  node->attrs.parsed = std::move(op);
-
-  // init and return NodeEntry
-  return nnvm::NodeEntry{node, 0, 0};
-}
-
-
-// Generator to create functions that convert mxnet layer operations
-// into a series of ngraph operations
-layerGraphs Compiler::create_layerGraphs() {
-  layerGraphs layer_funcs;
-  layer_funcs[std::string("Activation")] = [](const NodePtr node) {
-    Graph tmpGraph;
-    auto act_type = node->orig_node->attrs.dict["act_type"];
-    tmpGraph.AddNode(std::make_shared<OpNode>(node->orig_node, node->name,
-                                              act_type, node->inputs));
-    return tmpGraph;
-  };
-
-  return layer_funcs;
-}
-
 // Function that parses an nnvm Graph into an intermediary graph
-Graph Compiler::ParseNNVMGraph(nnvm::Graph& graph) {
-  // create inermediary graph
-  Graph tmpGraph;
+void Compiler::ParseNNVMGraph() {
   // Create dictionary of layer->ngraph functions
   auto layer_funcs = create_layerGraphs();
   // Use NNVM's depth first search to trace the tree and construct the
   // intermediary graph
-  nnvm::DFSVisit(graph.outputs, [&graph, &tmpGraph,
+  nnvm::DFSVisit(graph_.outputs, [this, 
                                  &layer_funcs](const nnvm::NodePtr node) {
-    const auto& idx = graph.indexed_graph();
+    const auto& idx = this->graph_.indexed_graph();
 
     const auto& mutable_nodes = idx.mutable_input_nodes();
     const uint32_t nid = idx.node_id(node.get());
     if (mutable_nodes.count(nid) != 0) {
       // add an auxillary node to the graph
-      tmpGraph.AddNode(std::make_shared<AuxNode>(node, node->attrs.name));
+      this->ngraph_.AddNode(std::make_shared<AuxNode>(node, node->attrs.name));
     } else if (node->is_variable()) {
       // add variable to the graph
-      tmpGraph.AddNode(std::make_shared<VariableNode>(node, node->attrs.name));
+      this->ngraph_.AddNode(std::make_shared<VariableNode>(node, node->attrs.name));
     } else {
       // create operation node
       auto op_name = clean_opname(node->op()->name);
@@ -192,52 +185,49 @@ Graph Compiler::ParseNNVMGraph(nnvm::Graph& graph) {
         const nnvm::NodeEntry& e = node->inputs[i];
         std::shared_ptr<Node> tmpnode;
         try {
-          tmpnode = tmpGraph[e.node->attrs.name];
+          tmpnode = this->ngraph_[e.node->attrs.name];
         } catch (std::string& error) {
           try {
             auto name = e.node->attrs.name + "_" + std::to_string(e.index);
-            tmpnode = tmpGraph[name];
+            tmpnode = this->ngraph_[name];
           } catch (std::string& error) {
             tmpnode = std::make_shared<VariableNode>(node, e.node->attrs.name);
-            tmpGraph.AddNode(tmpnode);
+            this->ngraph_.AddNode(tmpnode);
           }
         }
         op_node->inputs.emplace_back(tmpnode);
       }
-      auto expand_layers = [&layer_funcs](std::shared_ptr<OpNode>& op_node,
-                                          Graph& tmpGraph) {
+      auto expand_layers = [this, &layer_funcs](std::shared_ptr<OpNode>& op_node) {
         auto tmp = layer_funcs[op_node->operation](op_node);
         if (tmp.num_outputs > 1)
-          tmpGraph.nodes_.erase(std::remove(tmpGraph.nodes_.begin(),
-                                            tmpGraph.nodes_.end(), op_node),
-                                tmpGraph.nodes_.end());
+          this->ngraph_.nodes_.erase(std::remove(this->ngraph_.nodes_.begin(),
+                                            this->ngraph_.nodes_.end(), op_node),
+                                this->ngraph_.nodes_.end());
 
-        for (auto n : tmp.nodes_) tmpGraph.AddNode(n);
+        for (auto n : tmp.nodes_) this->ngraph_.AddNode(n);
       };
 
       if (layer_funcs.count(op_node->operation) != 0) {
         // perform layer expansions
-        expand_layers(op_node, tmpGraph);
+        expand_layers(op_node);
       } else {
         // add operation
-        tmpGraph.AddNode(op_node);
+        this->ngraph_.AddNode(op_node);
       }
     }
   });
 
   // get the shape and data types of all of the nodes
-  const auto& idx = graph.indexed_graph();
+  const auto& idx = graph_.indexed_graph();
   const auto inferred_shapes =
-      graph.GetAttr<std::vector<nnvm::TShape>>("shape");
-  const auto inferred_dtypes = graph.GetAttr<std::vector<int>>("dtype");
-  for (auto node : tmpGraph.nodes_) {
+      graph_.GetAttr<std::vector<nnvm::TShape>>("shape");
+  const auto inferred_dtypes = graph_.GetAttr<std::vector<int>>("dtype");
+  for (auto node : this->ngraph_.nodes_) {
     const uint32_t nid = idx.node_id(node->orig_node.get());
     const uint32_t eid = idx.entry_id(nid, 0);
     node->shape = inferred_shapes[eid];
     node->dtype = inferred_dtypes[eid];
   }
-  // return intermediary graph
-  return tmpGraph;
 }
 
 }  // end namespace ngraph
