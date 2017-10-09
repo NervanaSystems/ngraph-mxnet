f99b7cd427c94773ea389cb9fdaf4fcacd927f5d
ransford.m.hyman <ransford.m.hyman@intel.com>
2017-10-09 13:52:33 -0700

    Fixed the Segfault issue.  Used the Registered MXNet operations for my operation instead of creating my own from scratch.  Turns out that is not possible.  They must be registered first

--- tests/cpp/ngraph/test_ngraph_compiler.h
+++ tests/cpp/ngraph/test_ngraph_compiler.h
@@ -2,23 +2,28 @@
 #include "ngraph_compiler.h"
 #include <nnvm/graph.h>
 
+
 class CompilerTest: public ::testing::Test {
 
 protected:
 virtual void SetUp(){
-//The idea here is to create a graph for (A+B)
+//The idea here is to create a graph for (A*B) dot product
+//This has 2 inputs and one output
 //Initialize two input nodes
 nnvm::NodeAttrs attr;
+in1 = nnvm::Node::Create();
+in2 = nnvm::Node::Create();
+op_node = nnvm::Node::Create();
+attr.name = "in1";
+attr.name = "in1";
 attr.name = "in1";
 in1->attrs = attr;
 attr.name = "in2";
 in2->attrs = attr;
 //Create op node
-std::shared_ptr<nnvm::Op> temp_op;
-temp_op->name = "add";
-temp_op->set_num_inputs(2);
-temp_op->set_num_outputs(1);
-attr.op = temp_op.get();
+const nnvm::Op* temp_op = nnvm::Op::Get("dot");
+attr.op = temp_op;
+
 op_node->attrs = attr;
 
 nnvm::NodeEntry ne0;
@@ -36,8 +41,8 @@ nnvm_graph.outputs.push_back(ne2);
 
 virtual void TearDown(){};
 
-nnvm::NodePtr in1;
-nnvm::NodePtr in2;
+nnvm::NodePtr in1; 
+nnvm::NodePtr in2; 
 nnvm::NodePtr op_node;
 nnvm::Graph nnvm_graph;
 int temp = 1;
