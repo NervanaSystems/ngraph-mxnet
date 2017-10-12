7b840d9b8721d47d38564b6f962c7a4ae98dabf7
ransford.m.hyman <ransford.m.hyman@intel.com>
2017-10-12 14:42:50 -0700

    Small refactoring and style edits to prepare for PR

--- tests/cpp/ngraph/test_ngraph_ops.cc
+++ tests/cpp/ngraph/test_ngraph_ops.cc
@@ -2,11 +2,8 @@
 #include "ngraph_compiler.h"
 #include "test_ngraph_compiler.h"
 
-
-TEST_F(CompilerTest, TempValue ) {
-
-ngraph_bridge::Compiler compiler(nnvm_graph,feed_dict,inputs);
-ASSERT_EQ(temp,1);
+TEST_F(CompilerTest, TempValue) {
+  ngraph_bridge::Compiler compiler(nnvm_graph, feed_dict, inputs);
+  // Sample to illustrate Test Fixture usage
+  ASSERT_EQ(temp, 1);
 }
-
-
