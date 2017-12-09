e951f06c23caa57a458387f2c1167b3d2949cd15
Matthew Brookhart <matthew.i.brookhart@intel.com>
2017-12-08 17:02:27 -0800

    Change constant arrays to a scalar constant and broadcast (#97)

--- src/ngraph/ngraph_sgcompiler_utils.h
+++ src/ngraph/ngraph_sgcompiler_utils.h
@@ -70,9 +70,17 @@ inline ngraph::Shape TShape_to_NShape(const nnvm::TShape& inshape) {
 // along with a string representing the number
 inline std::shared_ptr<ngraph::Node> makeConstant(const NodePtr& node,
                                                   const std::string& num) {
-  const auto& et = getType(node->dtype_);
+  NgraphNodePtr val = std::make_shared<ngraph::op::Constant>(
+      getType(node->dtype_), ngraph::Shape{}, num);
   auto shape = TShape_to_NShape(node->shape_);
-  return std::make_shared<ngraph::op::Constant>(et, shape, num);
+
+  if (shape.size() > 0) {
+    ngraph::AxisSet axes;
+    for (size_t i = 0; i < shape.size(); i++) axes.insert(i);
+    val = std::make_shared<ngraph::op::Broadcast>(val, shape, axes);
+  }
+
+  return val;
 }
 
 // This function expects the input string to be of the form
