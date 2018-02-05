bf6e866becfa8c2a824aeecd82b780e7497749fa
ransford.m.hyman <ransford.m.hyman@intel.com>
2018-02-05 10:32:22 -0800

    Addressing PR review comments.  Changed the name of selected_device to something more familar to developers

--- tests/python/ngraph/test_abc.py
+++ tests/python/ngraph/test_abc.py
@@ -20,40 +20,39 @@ import sys
 
 
 def test_abc():
-    selected_device = mx.cpu()
+    context = mx.cpu()
     if args.is_nnp:
-        selected_device = mx.nnp()
-    with selected_device:
-        A = mx.symbol.Variable('A')
-        B = mx.symbol.Variable('B')
-        C = mx.symbol.Variable('C')
-        D = mx.symbol.Variable('D')
-        Avals = np.array([[1,2],[3,4]], dtype=np.float32)
-        Bvals = np.array([[5,6],[7,8]], dtype=np.float32)
-        Cvals = np.array([[9,10],[11,12]], dtype=np.float32)
-        Dvals = np.array([[0,0],[0,0]], dtype=np.float32)
-        Aarr = mx.nd.array(Avals)
-        Barr = mx.nd.array(Bvals)
-        Carr = mx.nd.array(Cvals)
-        Darr = mx.nd.array(Dvals)
-        out1 = (A+B)*C + D
-        exec1 = out1.bind(selected_device,args=[Aarr, Barr,Carr, Darr])
-        exec1.forward(is_train=False)
-        out1 = exec1.outputs[0].asnumpy()
-        assert (out1 == (Avals + Bvals) * Cvals+ Dvals).all()
-    
-    
-        out2 = (B+C)*A + D
-        exec2 = out2.bind(selected_device, args=[Barr, Carr, Aarr, Darr])
-        exec2.forward(is_train=False)
-        out2 = exec2.outputs[0].asnumpy()
-        assert (out2 == (Bvals + Cvals) * Avals+ Dvals).all()
-    
-        out3 = (C+A)*B + D
-        exec3 = out3.bind(selected_device,args=[Carr, Aarr, Barr, Darr])
-        exec3.forward(is_train=False)
-        out3 = exec3.outputs[0].asnumpy()
-        assert (out3 == (Cvals + Avals) * Bvals + Dvals).all()
+        context = mx.nnp()
+    A = mx.symbol.Variable('A')
+    B = mx.symbol.Variable('B')
+    C = mx.symbol.Variable('C')
+    D = mx.symbol.Variable('D')
+    Avals = np.array([[1,2],[3,4]], dtype=np.float32)
+    Bvals = np.array([[5,6],[7,8]], dtype=np.float32)
+    Cvals = np.array([[9,10],[11,12]], dtype=np.float32)
+    Dvals = np.array([[0,0],[0,0]], dtype=np.float32)
+    Aarr = mx.nd.array(Avals,ctx=context)
+    Barr = mx.nd.array(Bvals,ctx=context)
+    Carr = mx.nd.array(Cvals,ctx=context)
+    Darr = mx.nd.array(Dvals,ctx=context)
+    out1 = (A+B)*C + D
+    exec1 = out1.bind(context,args=[Aarr, Barr,Carr, Darr])
+    exec1.forward(is_train=False)
+    out1 = exec1.outputs[0].asnumpy()
+    assert (out1 == (Avals + Bvals) * Cvals+ Dvals).all()
+
+
+    out2 = (B+C)*A + D
+    exec2 = out2.bind(context, args=[Barr, Carr, Aarr, Darr])
+    exec2.forward(is_train=False)
+    out2 = exec2.outputs[0].asnumpy()
+    assert (out2 == (Bvals + Cvals) * Avals+ Dvals).all()
+
+    out3 = (C+A)*B + D
+    exec3 = out3.bind(context,args=[Carr, Aarr, Barr, Darr])
+    exec3.forward(is_train=False)
+    out3 = exec3.outputs[0].asnumpy()
+    assert (out3 == (Cvals + Avals) * Bvals + Dvals).all()
     
 
 if __name__ == '__main__':
