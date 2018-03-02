11badfa9efecae85a679a7a87c6f915cf290fbec
Matthew Brookhart <matthew.i.brookhart@intel.com>
2018-03-02 14:56:21 -0800

    passes CPU unit tests, don't pass python unit tests yet

--- src/ngraph/ngraph_nnvm_utils.h
+++ src/ngraph/ngraph_nnvm_utils.h
@@ -17,6 +17,7 @@
 #ifndef MXNET_NGRAPH_NGRAPH_NNVM_UTILS_H_
 #define MXNET_NGRAPH_NGRAPH_NNVM_UTILS_H_
 
+#include <mxnet/ndarray.h>
 #include <mxnet/op_attr_types.h>
 
 #include <algorithm>
@@ -42,17 +43,17 @@ inline size_t get_buffer_size(const T& shape, size_t nbytes) {
 // This function creates an ngraph Tensor from the shape and type
 // of an input mxnet TBlob. It optionally copies the data
 // from the TBlob to the ngraph tensor.
-inline std::shared_ptr<TensorView> TBlob_to_TensorView(
-    const mxnet::TBlob& input,
+inline std::shared_ptr<TensorView> NDArray_to_TensorView(
+    const mxnet::NDArray& input,
     std::shared_ptr<ngraph::runtime::Backend> backend, bool copy) {
-  auto shape = TShape_to_NShape(input.shape_);
-  const auto& element_type = getType(input.type_flag_);
+  auto shape = TShape_to_NShape(input.shape());
+  const auto& element_type = getType(input.dtype());
 
   auto TV = backend->make_primary_tensor_view(element_type, shape);
 
   if (copy) {
     auto buffer_size = get_buffer_size(shape, element_type.size());
-    TV->write(input.dptr_, 0, buffer_size);
+    TV->write(input.storage_handle().dptr, 0, buffer_size);
   }
 
   return TV;
@@ -63,55 +64,55 @@ inline std::shared_ptr<TensorView> TBlob_to_TensorView(
 // equialently shaped and typed ngraph tensors, optionally
 // copied the data from the TBlobs to ngraph
 inline TensorViewVector make_ngraph_placeholders(
-    const std::vector<mxnet::TBlob>& inputs,
+    const std::vector<mxnet::NDArray>& inputs,
     std::shared_ptr<ngraph::runtime::Backend> backend, bool copy_data) {
   TensorViewVector out;
   std::transform(inputs.begin(), inputs.end(), std::back_inserter(out),
-                 [copy_data, backend](const mxnet::TBlob& tb) {
-                   return TBlob_to_TensorView(tb, backend, copy_data);
+                 [copy_data, backend](const mxnet::NDArray& input) {
+                   return NDArray_to_TensorView(input, backend, copy_data);
                  });
   return out;
 }
 
 template <class T>
-inline void result_plus_TBlob(void* mxnet_tblob, void* ngraph_tv,
-                              size_t buffer_size) {
-  T* mxnet_tblob_tptr = static_cast<T*>(mxnet_tblob);
-  T* ngraph_tv_tptr = static_cast<T*>(ngraph_tv);
+inline void result_plus_NDArray(void* mxnet_ptr, void* ngraph_ptr,
+                                size_t buffer_size) {
+  T* mxnet_ptr_tptr = static_cast<T*>(mxnet_ptr);
+  T* ngraph_ptr_tptr = static_cast<T*>(ngraph_ptr);
   for (size_t i = 0; i < (buffer_size / sizeof(T)); ++i) {
-    *(mxnet_tblob_tptr + i) += *(ngraph_tv_tptr + i);
+    *(mxnet_ptr_tptr + i) += *(ngraph_ptr_tptr + i);
   }
 }
 
 // Utility function that copies all results from an
-// ngraph computation into the output TBlobs in mxnet
-inline void result_to_TBlob(
+// ngraph computation into the output NDArrays in mxnet
+inline void result_to_NDArray(
     const std::vector<std::shared_ptr<ngraph::runtime::TensorView>>& results,
     const std::vector<mxnet::OpReqType>& req,
-    const std::vector<mxnet::TBlob>& outputs) {
+    const std::vector<mxnet::NDArray>& outputs) {
   for (size_t i = 0; i < outputs.size(); ++i) {
     if (req[i] == mxnet::kNullOp) continue;
 
-    const auto& element_type = getType(outputs[i].type_flag_);
-    auto buffer_size = get_buffer_size(outputs[i].shape_, element_type.size());
+    const auto& element_type = getType(outputs[i].dtype());
+    auto buffer_size = get_buffer_size(outputs[i].shape(), element_type.size());
 
-    void* mxnet_tblob = outputs[i].dptr_;
+    void* mxnet_tblob = outputs[i].storage_handle().dptr;
     if (req[i] == mxnet::kAddTo) {
       void* ngraph_tv = malloc(buffer_size);
       results[i]->read(ngraph_tv, 0, buffer_size);
 
       if (element_type == ngraph::element::f32)
-        result_plus_TBlob<float>(mxnet_tblob, ngraph_tv, buffer_size);
+        result_plus_NDArray<float>(mxnet_tblob, ngraph_tv, buffer_size);
       else if (element_type == ngraph::element::f64)
-        result_plus_TBlob<double>(mxnet_tblob, ngraph_tv, buffer_size);
+        result_plus_NDArray<double>(mxnet_tblob, ngraph_tv, buffer_size);
       else if (element_type == ngraph::element::u8)
-        result_plus_TBlob<uint8_t>(mxnet_tblob, ngraph_tv, buffer_size);
+        result_plus_NDArray<uint8_t>(mxnet_tblob, ngraph_tv, buffer_size);
       else if (element_type == ngraph::element::i8)
-        result_plus_TBlob<int8_t>(mxnet_tblob, ngraph_tv, buffer_size);
+        result_plus_NDArray<int8_t>(mxnet_tblob, ngraph_tv, buffer_size);
       else if (element_type == ngraph::element::i32)
-        result_plus_TBlob<int32_t>(mxnet_tblob, ngraph_tv, buffer_size);
+        result_plus_NDArray<int32_t>(mxnet_tblob, ngraph_tv, buffer_size);
       else if (element_type == ngraph::element::i64)
-        result_plus_TBlob<int64_t>(mxnet_tblob, ngraph_tv, buffer_size);
+        result_plus_NDArray<int64_t>(mxnet_tblob, ngraph_tv, buffer_size);
 
       free(ngraph_tv);
     } else {
