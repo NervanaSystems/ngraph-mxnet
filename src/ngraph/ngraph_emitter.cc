f1ab318dea5317ae6b090f98c2db534e8a8f89d3
Matthew Brookhart <matthew.i.brookhart@intel.com>
2018-03-15 09:24:41 -0700

    default to mxnet batchnorm for non-mkldnn optmized channel dimensions

--- src/ngraph/ngraph_emitter.cc
+++ src/ngraph/ngraph_emitter.cc
@@ -789,7 +789,7 @@ void Emitter::CreateLayerOps() {
     // insert channel size at the proper index for the channel
     convert_shape.insert(convert_shape.begin() + channel_axis, channel_size);
 
-    if (data_shape_size == 4 && channel_axis == 1 &&
+    if (false && data_shape_size == 4 && channel_axis == 1 &&
         node->dtype_ == mshadow::kFloat32 &&
         exe_mode_ == GraphExeMode::kTrain && !use_global_stats) {
       auto BN = std::make_shared<ngraph::op::BatchNorm>(eps, ng_in_gamma,
