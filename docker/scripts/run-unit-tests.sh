89b71f5f07f14fc0be14ae519438f04f3bad4e2e
Lam Thuy Nguyen <lam.nguyen@intel.com>
2018-03-28 09:57:44 -0700

    Daily Validation : Unit Tests  (#184)
    * CI tests : Run all unit tests
    
    * Remove pip3

--- docker/scripts/run-unit-tests.sh
+++ docker/scripts/run-unit-tests.sh
@@ -24,5 +24,5 @@ cd "$HOME/ng-mx"
 cd python && sudo -E pip install -e . && cd ../
 
 ## Unit tests test_operator.py 
-cmd="OMP_NUM_THREADS=4 pytest -n 2 tests/python/unittest/test_operator.py --verbose --capture=no"
+cmd="OMP_NUM_THREADS=4 pytest -s -n 2 tests/python/unittest/test_operator.py --verbose --capture=no --junit-xml=result_test_operator.xml --junit-prefix=result_test_operator"
 eval $cmd
