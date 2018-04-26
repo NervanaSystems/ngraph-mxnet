f1fdb2c60c703483b201bb476a6d0c8b3cb8de01
Ashok Emani <ashok.emani@intel.com>
2018-04-26 01:19:11 -0700

    fix layout handling

--- src/ndarray/ndarray.cc
+++ src/ndarray/ndarray.cc
@@ -736,7 +736,7 @@ std::shared_ptr<ngraph::runtime::TensorView> &NDArray::create_tensor_view() {
     CHECK(backend != nullptr);
     ptr_->tensor_view_ = backend->create_tensor(
         ngraph_bridge::getType(dtype_), ngraph_bridge::TShape_to_NShape(shape_),
-        ptr_->shandle.dptr);
+        storage_handle().dptr);
   }
   return ptr_->tensor_view_;
 }
