9b605a3e5b195c9d99254e3f88f467f4655a2240
Lam Thuy Nguyen <lam.nguyen@intel.com>
2018-05-02 22:12:43 -0700

    Remove debugs

--- docker/scripts/run-ng-mx-build-and-unit-tests.sh
+++ docker/scripts/run-ng-mx-build-and-unit-tests.sh
@@ -121,8 +121,6 @@ PS2='prompt-more> '
 virtualenv -p "${PYTHON_BIN_PATH}" "${venv_dir}"
 source "${venv_dir}/bin/activate"
 cd "$HOME/ng-mx/docker/scripts/"
-export PYTHON_BIN_PATH=$PYTHON_BIN_PATH:/tmp/venv_python${PYTHON_VERSION_NUMBER}/bin/python${PYTHON_VERSION_NUMBER}
-echo "The python bin path: ${PYTHON_BIN_PATH}"
 ./run-ng-mx-unit-tests.sh 2>&1 | tee ../mx-tests.log
 echo "===== Unit Tests Pipeline Exited with $? ====="
 
