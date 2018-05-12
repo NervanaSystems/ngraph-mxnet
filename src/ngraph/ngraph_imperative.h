8bf82cd35a12eb371b7458b99ff6c3922c0eee65
Matthew Brookhart <matthew.i.brookhart@intel.com>
2018-05-11 19:43:57 -0700

    NGRAPH-1796 Gluon bug fixes (#233)
    * make gluon use log functions instead of variables
    
    * fix bug in debug printing
    
    * fix convolution grouping
    
    * skip BN and zeros_like from gluon imperative

--- src/ngraph/ngraph_imperative.h
+++ src/ngraph/ngraph_imperative.h
@@ -53,8 +53,8 @@ class NGImperative : public Compiler {
     static std::unordered_set<std::string> layer_and_other{"split",
                                                            "SliceChannel"};
 
-    static std::unordered_set<std::string> skip_imperative{"expand_dims",
-                                                           "_copy", "_zeros"};
+    static std::unordered_set<std::string> skip_imperative{
+        "expand_dims", "_copy", "_zeros", "zeros_like", "BatchNorm"};
 
     if (skip_imperative.count(op_name)) return false;
 
