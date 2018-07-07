a669e99df78b8cb151783626d0b987d10448e8ca
Matthew Brookhart <matthew.i.brookhart@intel.com>
2018-07-07 09:37:23 -0700

    autodiff-based deconvolution (#282)
    * autodiff-based deconvolution
    
    * update cachable ops
    
    * Add Deconvolution bias
    
    * throw an error if the users tries to run with untested parameters
    
    * refactor for partially unsupported ops
    
    * turn for loop into find_if
    
    * turn a lambda into a utility

--- src/ngraph/ngraph_graph.cc
+++ src/ngraph/ngraph_graph.cc
@@ -365,8 +365,8 @@ void CollapseSubgraph(Graph* graph, int subgraph_num) {
     }
     for (auto node : tmpGraph->nodes_) {
       if (node->operation_ == "Convolution" ||
+          node->operation_ == "Deconvolution" ||
           node->operation_ == "BatchNorm" ||
-          node->operation_ == "SoftmaxOutput" ||
           node->operation_ == "FullyConnected") {
         for (size_t i = 1; i < node->inputs_.size(); ++i) {
           auto input = node->inputs_[i];
