7e9ef12c7333c17fbc818d079898e9863ca3e1b0
Ashok Emani <ashok.emani@intel.com>
2018-07-09 12:09:13 -0700

    disable mem-use for NNP

--- src/ngraph/ngraph_graph.h
+++ src/ngraph/ngraph_graph.h
@@ -202,7 +202,7 @@ class Graph : public Node {
         context_(context),
         enable_fprop_cache(enable_fprop_cache) {
     fprop_cache = std::make_shared<ngraph::FpropCache>();
-    is_reuse_mem = context.dev_type != mxnet::Context::kCPU;
+    is_reuse_mem = context.dev_type != mxnet::Context::kNNP;
   }
 
   std::string createNodeLabel() override {
