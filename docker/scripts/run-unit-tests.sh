fca84a88bc9f860ba69d0ac1cef42bc9eaea2235
Lam Thuy Nguyen <lam.nguyen@intel.com>
2018-08-21 10:30:22 -0700

    Revert

--- docker/scripts/run-unit-tests.sh
+++ docker/scripts/run-unit-tests.sh
@@ -17,22 +17,16 @@
 #!/bin/bash
 #Author:  Lam Nguyen
 set -u
-#set -e
+set -e
 
 cd "$HOME/ng-mx"
 
-# cd python && sudo -E pip install -e . && cd ../
-
 cd python && pip install -e . && pip install pytest nose scipy==1.0.0 &&  cd ../
-#pip install pytest
-#pip install nose
-#pip install scipy==1.0.0
 
 ## Unit tests test_operator.py 
 cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/unittest/test_operator.py --verbose --capture=no --junit-xml=result_test_operator.xml --junit-prefix=result_test_operator"
-echo $cmd
+
 eval $cmd
 echo "Result= "
 echo $?
-#OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/unittest/test_operator.py --verbose --capture=no --junit-xml=result_test_operator.xml --junit-prefix=result_test_operator
 exit $?
