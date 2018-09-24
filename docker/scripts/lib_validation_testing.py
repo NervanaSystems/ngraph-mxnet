90373817e63c0820833e84499a6f27aac5d2b6fc
Lam Thuy Nguyen <lam.nguyen@intel.com>
2018-09-24 10:32:31 -0700

    Add SSD and Mask RCNN Resnet50 GluonCV (#427)
    * Add SSD_VGG
    
    * Add mask_rcnn_resnet50_gluoncv
    
    * Fix issues
    
    * Remove Debug lines

--- docker/scripts/lib_validation_testing.py
+++ docker/scripts/lib_validation_testing.py
@@ -474,7 +474,6 @@ def runA3CDeepMarkScript(sourceDir=None,
 
 # End: def runA3CDeepMarkScript()
 
-
 def runSDDMobileNetDeepMarkScript(sourceDir=None,
                     script=None,          # Script to run
                    logID='',
@@ -502,6 +501,60 @@ def runSDDMobileNetDeepMarkScript(sourceDir=None,
     return runLog
 
 # End: def runSDDMobileNetDeepMarkScript()
+
+def runSSDDeepMarkScript(sourceDir=None,
+                    script=None,          # Script to run
+                   logID='',
+                   ompNumThreads=None,
+                   kmpAff=None,
+                   kmpBlocktime=None,
+                   batchsize=None,
+                   checkAccurary=None):            # Log line prefix
+
+    print("DeepMark script being run with:")
+    print("    script:         {}".format(str(script)))
+    print("    logID:          {}".format(str(logID)))
+
+    print("Setting up run in nGraph environment")
+    print("The Python version is: {}".format(os.environ['PYTHON_VERSION_NUMBER']))
+    process = subprocess.check_output(["which python"],shell=True)
+    python_lib = process.decode('utf-8').split()[0]
+    if checkAccurary:
+        cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network vgg16-reduced --is-train true --batch-size {} --accuracy-check".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
+        print("The command for checking inference accuracy is: {}".format(cmd))
+    else:
+        cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network vgg16-reduced --is-train true --batch-size {}".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
+        print("The command for checking inference performance is: {}".format(cmd))
+    runLog = runCommand(command=cmd, logID=logID)
+    return runLog
+# End: def runSDDMobileNetDeepMarkScript
+
+def runMASKRCNNGLUONCVDeepMarkScript(sourceDir=None,
+                    script=None,          # Script to run
+                   logID='',
+                   ompNumThreads=None,
+                   kmpAff=None,
+                   kmpBlocktime=None,
+                   batchsize=None,
+                   checkAccurary=None):            # Log line prefix
+
+    print("DeepMark script being run with:")
+    print("    script:         {}".format(str(script)))
+    print("    logID:          {}".format(str(logID)))
+
+    print("Setting up run in nGraph environment")
+    print("The Python version is: {}".format(os.environ['PYTHON_VERSION_NUMBER']))
+    process = subprocess.check_output(["which python"],shell=True)
+    python_lib = process.decode('utf-8').split()[0]
+    if checkAccurary:
+        cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network mask_rcnn_resnet50_v1b_coco --batch-size {} --accuracy-check".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
+        print("The command for checking inference accuracy is: {}".format(cmd))
+    else:
+        cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network mask_rcnn_resnet50_v1b_coco --batch-size {}".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
+        print("The command for checking inference performance is: {}".format(cmd))
+    runLog = runCommand(command=cmd, logID=logID)
+    return runLog
+# End: def runMASKRCNNGLUONCVDeepMarkScript()
 
 def runSockeyeGNMTDeepMarkScript(sourceDir=None,
                     script=None,          # Script to run
