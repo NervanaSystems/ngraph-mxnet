53584ff0b8891b252ee8fec17293e0479b516dc7
mbrookhart <matthew.i.brookhart@intel.com>
2018-10-04 11:18:06 -0700

    move tests to ngraph folder

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -781,6 +781,7 @@ unittest_ubuntu_cpu_ngraph() {
     export MXNET_STORAGE_FALLBACK_LOG_VERBOSE=0
     export LD_LIBRARY_PATH=/work/mxnet/lib:$LD_LIBRARY_PATH
     nosetests-3.4 $NOSE_COVERAGE_ARGUMENTS --with-xunit --xunit-file nosetests_unittest.xml --verbose tests/python/unittest -e "test_subgraph_exe" -e "test_make_subgraph" -e "test_monitor" -e "test_op_output_names_monitor"
+    nosetests-3.4 $NOSE_COVERAGE_ARGUMENTS --with-xunit --xunit-file nosetests_ngraph.xml --verbose tests/python/ngraph
 }
 
 # quantization gpu currently only runs on P3 instances
