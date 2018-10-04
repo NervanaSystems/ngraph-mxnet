72ee4d64ff390a14eed69cb68c26c6663a917b37
mbrookhart <matthew.i.brookhart@intel.com>
2018-10-04 10:47:21 -0700

    fix license header

--- tests/python/unittest/test_ngraph.py
+++ tests/python/unittest/test_ngraph.py
@@ -1,18 +1,19 @@
-#*******************************************************************************
-# Copyright 2018 Intel Corporation
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#*******************************************************************************
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 from __future__ import print_function
 import numpy as np
@@ -32,7 +33,8 @@ def binary_op_ex(sym, x_shape, y_shape):
     exe.set_monitor_callback(get_output_names_callback)
     mx_out = exe.forward(is_train=True, x=x_npy, y=y_npy)[0].asnumpy()
     exe.backward()
-    if os.environ['MXNET_SUBGRAPH_BACKEND'] == "ngraph":
+    if ('MXNET_SUBGRAPH_BACKEND' in os.environ and 
+        os.environ['MXNET_SUBGRAPH_BACKEND'] == "ngraph"):
         assert any(['ngraph' in name for name in output_names])
     return mx_out
 
