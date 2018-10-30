99c37cc2aa75a2111c41e91396604152a3fdf4e5
Lam Nguyen <lam.nguyen@intel.com>
2018-10-30 11:05:46 -0700

    Enable more unit tests

--- docker/scripts/run-unit-tests-gpu.sh
+++ docker/scripts/run-unit-tests-gpu.sh
@@ -26,10 +26,38 @@ cd python && pip install -e . && pip install pytest nose scipy &&  cd ../
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/stubs/:/usr/local/cuda-9.2/lib64/stubs/:/usr/local/nvidia/lib64/libcuda.so.1:/usr/local/cuda-9.0/lib64/stubs/
 echo " LD_LIBRARY_PATH ==== ${LD_LIBRARY_PATH}"
 
-## Unit tests test_device.py 
+## 1. Unit tests test_device.py
 cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_device.py --verbose --capture=no --junit-xml=result_test_device_gpu.xml --junit-prefix=result_test_device_gpu"
 eval $cmd
 
-## Unit tests test_operator.py 
-cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_device.py --verbose --capture=no --junit-xml=result_test_operator_gpu.xml --junit-prefix=result_test_operator_gpu"
+## 2. Unit tests test_operator_gpu.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_operator_gpu.py --verbose --capture=no --junit-xml=result_test_operator_gpu.xml --junit-prefix=result_test_operator_gpu"
+eval $cmd
+
+## 3. Unit tests test_forward.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_forward.py --verbose --capture=no --junit-xml=result_test_forward_gpu.xml --junit-prefix=result_test_forward_gpu"
+eval $cmd
+
+## 4. Unit tests test_gluon_gpu.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_gluon_gpu.py --verbose --capture=no --junit-xml=result_test_gluon_gpu.xml --junit-prefix=result_test_gluon_gpu"
+eval $cmd
+
+## 5. Unit tests test_gluon_model_zoo_gpu.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_gluon_model_zoo_gpu.py --verbose --capture=no --junit-xml=result_test_gluon_model_zoo_gpu.xml --junit-prefix=result_test_gluon_model_zoo_gpu"
+eval $cmd
+
+## 6. Unit tests test_kvstore_gpu.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_kvstore_gpu.py --verbose --capture=no --junit-xml=result_test_kvstore_gpu.xml --junit-prefix=result_test_kvstore_gpu"
+eval $cmd
+
+## 7. Unit tests test_nccl.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_nccl.py --verbose --capture=no --junit-xml=result_test_nccl_gpu.xml --junit-prefix=result_test_nccl_gpu"
+eval $cmd
+
+## 8. Unit tests test_rtc.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_rtc.py --verbose --capture=no --junit-xml=result_test_rtc_gpu.xml --junit-prefix=result_test_rtc_gpu"
+eval $cmd
+
+## 9. Unit tests test_tvm_brigde.py
+cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_tvm_brigde.py --verbose --capture=no --junit-xml=result_test_tvm_brigde_gpu.xml --junit-prefix=result_test_tvm_brigde_gpu"
 eval $cmd
