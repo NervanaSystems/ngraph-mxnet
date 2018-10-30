165188d2f2f56a40d2b5a1b183618946febfa22f
Lam Nguyen <lam.nguyen@intel.com>
2018-10-29 19:14:49 -0700

    Added more comments

--- docker/scripts/run-unit-tests-gpu.sh
+++ docker/scripts/run-unit-tests-gpu.sh
@@ -21,7 +21,10 @@ set -e
 
 cd "$HOME/ng-mx"
 
-cd python && pip install -e . && pip install pytest nose scipy==1.0.0 &&  cd ../
+echo "Print the lib : ${HOME}"
+echo `ls ${HOME}/ng-mx/lib`
+
+cd python && pip install -e . && pip install pytest nose scipy &&  cd ../
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/stubs/:/usr/local/cuda-9.2/lib64/stubs/:/usr/local/nvidia/lib64/libcuda.so.1:/usr/local/cuda-9.0/lib64/stubs/
 echo " LD_LIBRARY_PATH ==== ${LD_LIBRARY_PATH}"
@@ -30,7 +33,7 @@ echo " LD_LIBRARY_PATH ==== ${LD_LIBRARY_PATH}"
 sudo ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
 
 #sudo ln -s /usr/local/cuda-8.0/lib64/stubs/libcuda.so /usr/local/cuda-8.0/lib64/stubs/libcuda.so.1
-
+cmd="OMP_NUM_THREADS=4 $(which python) tests/python/gpu/test_device.py"
 ## Unit tests test_operator.py 
-cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_device.py --verbose --capture=no --junit-xml=result_test_operator_gpu.xml --junit-prefix=result_test_operator_gpu"
+#cmd="OMP_NUM_THREADS=4 $(which python) -m pytest -s tests/python/gpu/test_device.py --verbose --capture=no --junit-xml=result_test_operator_gpu.xml --junit-prefix=result_test_operator_gpu"
 eval $cmd
