f6c49bcbf87f2707bc13631aec2e9b9fe8f22c9e
mbrookhart <matthew.i.brookhart@intel.com>
2018-11-13 15:09:52 -0800

    add nullptr checks

--- src/ndarray/ndarray.cc
+++ src/ndarray/ndarray.cc
@@ -746,20 +746,20 @@ std::shared_ptr<ngraph::runtime::Tensor> &NDArray::create_tensor(
       ptr_->tensor_view_->get_shape() !=
           ngraph_bridge::TShape_to_NShape(shape_)) {
     auto backend = ngraph_bridge::GetBackendFromContext(ctx());
+    void* dptr = storage_handle().dptr;
     CHECK(backend != nullptr);
+    CHECK(dptr != nullptr);
     ngraph::Shape shape{};
     if (!is_scalar) {
       shape = ngraph_bridge::TShape_to_NShape(shape_);
     }
     if (is_boolean) {
-      ptr_->tensor_view_ = backend->create_tensor(
-          ngraph::element::boolean, shape,
-          storage_handle().dptr);
+      ptr_->tensor_view_ =
+          backend->create_tensor(ngraph::element::boolean, shape, dptr);
 
     } else {
-      ptr_->tensor_view_ = backend->create_tensor(
-          ngraph_bridge::getType(dtype_), shape,
-          storage_handle().dptr);
+      ptr_->tensor_view_ =
+          backend->create_tensor(ngraph_bridge::getType(dtype_), shape, dptr);
     }
   }
   return ptr_->tensor_view_;
