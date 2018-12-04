fe46a66a025f370997c3f3a4fe46ac7034e44a9c
Lam Thuy Nguyen <lam.nguyen@intel.com>
2018-12-04 09:33:26 -0800

    Update accuracy check for ssd_vgg16_300 (#511)
    * Update accuracy check for ssd_vgg16_300
    
    * Update test_deepmark_ssd_inference.py

--- docker/scripts/lib_validation_testing.py
+++ docker/scripts/lib_validation_testing.py
@@ -632,15 +632,12 @@ def runSSDDeepMarkScript(sourceDir=None,
     process = subprocess.check_output(["which python"],shell=True)
     python_lib = process.decode('utf-8').split()[0]
     if checkAccurary:
-        cmd = ""
-        #cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network ssd-vgg16 --batch-size {} --accuracy-check".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
-        #print("The command for checking inference accuracy is: {}".format(cmd))
-        print("Disable the test case.NGRAPH-3314")
-        runLog = ""
+        cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network ssd_300_vgg16_atrous_voc --batch-size {} --accuracy-check".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
+        print("The command for checking inference accuracy for ssd_300_vgg16_atrous_voc is: {}".format(cmd))
     else:
         cmd = "OMP_NUM_THREADS={} KMP_AFFINITY={} KMP_BLOCKTIME={} {} {} --network ssd-vgg16 --batch-size {}".format(ompNumThreads, kmpAff, kmpBlocktime, python_lib.strip(), script, batchsize.strip())
         print("The command for checking inference performance is: {}".format(cmd))
-        runLog = runCommand(command=cmd, logID=logID)
+    runLog = runCommand(command=cmd, logID=logID)
     return runLog
 # End: def
 
